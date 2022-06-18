1302ad3acaeeb1bd78034c41a3a2bb5496b7d8b7
DreVinciCode <dre.vivero@gmail.com>
2022-06-18 00:13:50 -0400

    Create and Join Components

---
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField CreateInput;
+    public TMP_InputField JoinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(CreateInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+
+}
